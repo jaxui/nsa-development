b203708e9d74e9d9b6d72e3643f5e3f1f709aba1
JAXUI <93093913+jaxui@users.noreply.github.com>
2021-10-24 15:27:01 -0400

    Update Nsaclient.java

--- src/main/java/me/jaxui/nsaclient/Nsaclient.java
+++ src/main/java/me/jaxui/nsaclient/Nsaclient.java
@@ -12,7 +12,7 @@ public class ExampleMod
 {
     public static final String MODID = "nsaclient";
     public static final String NAME = "Nsa Client";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "beta0.1";
 
     private static Logger logger;
 
